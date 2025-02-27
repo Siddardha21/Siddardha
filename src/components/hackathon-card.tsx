cdf7afbda3b95378166a6bff9660e2a253c3f856
Siddardha LS <59073066+Siddardha21@users.noreply.github.com>
2025-02-26 21:51:45 -0500

    updated 4

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -7,16 +7,11 @@ export function HackathonCard({
   title,
   description,
   dates,
-  image,
   links,
 }: Props) {
   return (
     <li className="relative ml-10 py-4">
       <div className="absolute -left-16 top-2 flex items-center justify-center bg-white rounded-full">
-        <Avatar className="border size-12 m-auto">
-          <AvatarImage src={image} alt={title} className="object-contain" />
-          <AvatarFallback>{title[0]}</AvatarFallback>
-        </Avatar>
       </div>
       <div className="flex flex-1 flex-col justify-start gap-1">
         {dates && (
